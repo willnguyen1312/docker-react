e8fc08c16bc566a4f18f1bbea09e47baabbad938
Nam Nguyen <nam.nguyen.node@gmail.com>
2019-12-13 19:04:24 +0700

    WIP

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Golang!!!
+          Learn Golang - WIP!!!
         </a>
       </header>
     </div>
